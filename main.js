39501b2cc641a8ccbf8ca1bd6baad9bdb625ad3a
ShivamParikh1 <166666120+ShivamParikh1@users.noreply.github.com>
2025-06-27 21:33:23 -0400

    Fix Firebase config import paths

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import './script.js'
-import './firebase-config.js'
+import './src/firebase-config.js'
 
 // The app is defined in index.html and controlled by script.js
 console.log('HabitFlow app initialized');
