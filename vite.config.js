d1d54998e9da9c11aa5024e99c8ac0f8b4a02627
ShivamParikh1 <166666120+ShivamParikh1@users.noreply.github.com>
2025-07-02 11:51:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/3njvyhyj/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
