2cfe0881157bc37832846f1d1bc88290d0d396e0
VYOE123 <vyoecolumbus@gmail.com>
2025-07-02 14:27:46 -0400

    Fix Netlify deployment routing and base path issues

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-// Get the base URL for GitHub Pages
-const basename = import.meta.env.DEV ? '/' : '/3njvyhyj/'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
