4929292ecf498727208e676523d7848b8b6972a0
ShivamParikh1 <166666120+ShivamParikh1@users.noreply.github.com>
2025-07-03 14:21:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HabitAppV3/',
+  base: '/HabitAppV5/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
